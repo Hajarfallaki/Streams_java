d9a499a24dc881655209db961742036676762a3a
hajar_elfallaki_idrissi <hajarfallaki24@gmail.com>
2025-01-06 16:38:24 +0100

    Modification des fichiers APP2.java et words.txt

--- src/APP2.java
+++ src/APP2.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class APP2 {
+    public static void main(String[] args) {
+        try {
+            FileReader fr = new FileReader("./words.txt");
+            FileWriter fw = new FileWriter("./wordsCopies.txt");
+
+            int c;
+            while ((c = fr.read()) != -1) {
+                fw.write((char) c); // Cast to char before writing
+            }
+            fr.close();
+            fw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
