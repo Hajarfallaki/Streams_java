cbbf937da0619b4ef8760f38282fccbeaff1b446
hajar_elfallaki_idrissi <hajarfallaki24@gmail.com>
2025-01-06 15:50:09 +0100

    Ajout d'un programme Java pour vérifier les propriétés d'un chemin et explorer le contenu d'un répertoire.

--- src/AP1.java
+++ src/AP1.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.io.File;
 
 public class AP1 {
+    public static void main(String[] args) {
+        File file=new File("C://Users/hajar/OneDrive/Bureau/enspec_club");
+        System.out.println(file.isDirectory()?"Dossier":"Fichier");
+        System.out.print(file.getAbsolutePath());
+        System.out.println();
+        System.out.print(file.canRead()?"r":"-");
+        System.out.print(file.canWrite()?"w":"-");
+        System.out.print(file.canExecute()?"x":"-");
+        for(File file1:file.listFiles()){
+            System.out.print(file1.isDirectory()?"d":"-");
+            System.out.print(" " +file1.getName());
+            System.out.println(" "+file1.length());
+
+        }
+    }
 }
